7eb3bd7500f9150ba5472020f9de677dc175d119
CasualCassowary <brodycrosby@yahoo.com>
2021-09-09 13:03:20 -0400

    Create config.js

---
+++ module/config.js
@@ -0,0 +1,130 @@
+export const pokerpg = {};
+
+pokerpg.attributes = {
+	"str": "pokerpg.AttributeStr",
+	"agl": "pokerpg.AttributeAgl",
+	"vit": "pokerpg.AttributeVit",
+	"spt": "pokerpg.AttributeSpt",
+	"awe": "pokerpg.AttributeAwe",
+	"int": "pokerpg.AttributeInt"
+};
+
+pokerpg.attributeAbbreviations = {
+	"str": "pokerpg.AttributeStrAbbr",
+	"agl": "pokerpg.AttributeAglAbbr",
+	"vit": "pokerpg.AttributeVitAbbr",
+	"spt": "pokerpg.AttributeSptAbbr",
+	"awe": "pokerpg.AttributeAweAbbr",
+	"int": "pokerpg.AttributeIntAbbr"
+};
+
+pokerpg.characterStats = {
+	"atk": "pokerpg.characterStatAtk",
+	"def": "pokerpg.characterStatDef",
+	"spAtk": "pokerpg.characterStatSpAtk",
+	"spDef": "pokerpg.characterStatSpDef",
+	"spd": "pokerpg.characterStatSpd",
+	"hp": "pokerpg.characterStatHp"
+};
+
+pokerpg.characterBaseStats = {
+	"baseAtk": "pokerpg.characterBaseStatAtk",
+	"baseDef": "pokerpg.characterBaseStatDef",
+	"baseSpAtk": "pokerpg.characterBaseStatSpAtk",
+	"baseSpDef": "pokerpg.characterBaseStatSpDef",
+	"baseSpd": "pokerpg.characterBaseStatSpd",
+	"baseHp": "pokerpg.characterBaseStatHp"
+};
+
+pokerpg.evasion = "eva": "pokerpg.evasion";
+
+pokerpg.saves = {
+	"body": "pokerpg.saveBody",
+	"reflex": "pokerpg.saveReflect",
+	"mind": "pokerpg.saveMind"
+};
+
+pokerpg.pokemonStats = {
+	"atk": "pokerpg.pokemonStatAtk",
+	"def": "pokerpg.pokemonStatDef",
+	"spAtk": "pokerpg.pokemonStatSpAtk",
+	"spDef": "pokerpg.pokemonStatSpDef",
+	"spd": "pokerpg.pokemonStatSpd",
+	"hp": "pokerpg.pokemonStatHp"
+};
+
+pokerpg.pokemonBaseStats = {
+	"baseAtk": "pokerpg.pokemonBaseStatAtk",
+	"baseDef": "pokerpg.pokemonBaseStatDef",
+	"baseSpAtk": "pokerpg.pokemonBaseStatSpAtk",
+	"baseSpDef": "pokerpg.pokemonBaseStatSpDef",
+	"baseSpd": "pokerpg.pokemonBaseStatSpd",
+	"baseHp": "pokerpg.pokemonBaseStatHp"
+};
+
+pokerpg.skills = {
+	"acr": "pokerpg.skillAcr",
+	"ath": "pokerpg.skillAth",
+	"bre": "pokerpg.skillBre",
+	"com": "pokerpg.skillCom",
+	"dec": "pokerpg.skillDec",
+	"end": "pokerpg.skillEnd",
+	"aid": "pokerpg.skillAid",
+	"his": "pokerpg.skillHis",
+	"ins": "pokerpg.skillIns",
+	"itd": "pokerpg.skillItd",
+	"inv": "pokerpg.skillInv",
+	"mec": "pokerpg.skillMec",
+	"mys": "pokerpg.skillMys",
+	"pkc": "pokerpg.skillPkc",
+	"pok": "pokerpg.skillPok",
+	"pcn": "pokerpg.skillPer",
+	"psn": "pokerpg.skillPsn",
+	"pss": "pokerpg.skillPss",
+	"sci": "pokerpg.skillSci",
+	"ste": "pokerpg.skillSte",
+	"str": "pokerpg.skillStr",
+	"sty": "pokerpg.skillSty",
+	"sur": "pokerpg.skillSur",
+	"tra": "pokerpg.skillTra"
+};
+
+
+
+pokerpg.characterExpLvl = [
+	0,200,400,650,950,1250,1600,2000,2400,2800
+];
+
+pokerpg.pokemonExpLvl = [
+	0,300,800,1400,2100,2800,3700,4700,5800,7000,8300,9700,
+	11200,12800,14500,16300,17900,19900,21000,23100
+];
+
+pokerpg.pokemonTypes = {
+	"nrm": "pokerpg.pokemonTypeNrm",
+	"grs": "pokerpg.pokemonTypeGrs",
+	"fir": "pokerpg.pokemonTypeFir",
+	"wat": "pokerpg.pokemonTypeWat",
+	"rck": "pokerpg.pokemonTypeRck",
+	"grd": "pokerpg.pokemonTypeGrd",
+	"ftg": "pokerpg.pokemonTypeFtg",
+	"bug": "pokerpg.pokemonTypeBug",
+	"psn": "pokerpg.pokemonTypePsn",
+	"elc": "pokerpg.pokemonTypeElc",
+	"fly": "pokerpg.pokemonTypeFly",
+	"psy": "pokerpg.pokemonTypePsy",
+	"drk": "pokerpg.pokemonTypeDrk",
+	"gho": "pokerpg.pokemonTypeGho",
+	"ice": "pokerpg.pokemonTypeIce",
+	"fae": "pokerpg.pokemonTypeFae",
+	"stl": "pokerpg.pokemonTypeStl",
+	"drg": "pokerpg.pokemonTypeDrg"
+};
+
+pokerpg.attackTypes = {
+	"phy": "pokerpg.attackTypePhy",
+	"spe": "pokerpg.attackTypeSpe",
+	"sta": "pokerpg.attackTypeSta"
+}
+
+
